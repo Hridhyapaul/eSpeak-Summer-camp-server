3f694e91d33778c441151c9c12bc937bf732c64c
Hridhyapaul <hpdesign2021@gmail.com>
2023-06-09 20:46:04 +0600

    Created get api to find all classes

--- index.js
+++ index.js
@@ -28,47 +28,13 @@ async function run() {
         const courseCollection = client.db("eSpeakDB").collection("courses");
         const cartCollection = client.db("eSpeakDB").collection("carts");
         const usersCollection = client.db("eSpeakDB").collection("users");
+
+        app.get('/courses', async (req, res) => {
+            const result = await courseCollection.find().sort({ enrolled: -1 }).toArray()
+            res.send(result)
+        })
+
         
-        app.get('/courses', async(req, res) => {
-            const result = await courseCollection.find().sort({enrolled: -1}).toArray()
-            res.send(result)
-        })
-
-        // User collection related api....
-
-        app.post('/users', async(req, res) => {
-            const user = req.body;
-            const result = await usersCollection.insertOne(user)
-            res.send(result)
-        })
-
-        // Cart Collection related api....
-
-        app.get('/carts', async(req, res) => {
-            const email = req.query.email
-            if(!email){
-                res.send([])
-            }
-            const query = {email: email};
-            const result = await cartCollection.find(query).toArray();
-            res.send(result)
-        })
-
-        app.post('/carts', async(req, res) => {
-            const item = req.body;
-            console.log(item)
-            const result = await cartCollection.insertOne(item)
-            res.send(result)
-        })
-
-        app.delete('/carts/:id', async(req, res) => {
-            const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
-            const result = await cartCollection.deleteOne(query);
-            res.send(result);
-        })
-
-
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
